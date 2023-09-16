a6bbf485d321de7b9e4abe2022acad85657d762a
Sdizy <saohatsesamkelo@gmail.com>
2023-09-16 01:14:52 -0700

    main.h

--- main.h
+++ main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
-void putchar(char c);
+void _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
