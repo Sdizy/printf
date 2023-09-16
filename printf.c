1db954db222c2f3f7b4cec5b8139db3e8178313c
Kisakye19 <kisakyepesther@gmail.com>
2023-09-16 04:14:19 -0700

    question 0

--- printf.c
+++ printf.c
@@ -1,5 +1,12 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
+
+/**
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
+ */
 
 int _printf(const char *format, ...)
 {
