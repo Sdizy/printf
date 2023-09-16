eec0c42fb19bfee6df8c158269a275b05a10a7c5
Sdizy <saohatsesamkelo@gmail.com>
2023-09-16 13:47:08 -0700

    Pr

--- printf.c
+++ printf.c
@@ -5,55 +5,54 @@
 /**
  * _printf - Custom printf function
  * @format: The format string
- *
  * Return: The number of characters printed (excluding the null byte)
  */
+
 int _printf(const char *format, ...)
-
 {
-    int printed_chars = 0; 
-    va_list args; 
+int printed_chars = 0;
+va_list args;
 
-    va_start(args, format);
+ va_start(args, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            putchar(*format); 
-            printed_chars++;
-        }
-        else
-        {
-            format++; 
+while (*format)
+{
+if (*format != '%')
+{
+putchar(*format);
+printed_chars++;
+}
+else
+{
+format++;
 
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int); 
-                putchar(c);
-                printed_chars++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *); 
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    putchar(*str); 
-                    printed_chars++;
-                    str++;
-                }
-            }
-            else if (*format == '%')
-            {
-                putchar('%'); 
-   		printed_chars++;
-            }
-            format++;
-        }
-    }
+if (*format == 'c')
+{
+char c = va_arg(args, int);
+putchar(c);
+printed_chars++;
+}
+else if (*format == 's')
+{
+char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+putchar(*str);
+printed_chars++;
+str++;
+}
+}
+else if (*format == '%')
+{
+putchar('%');
+printed_chars++;
+}
+format++;
+}
+}
 
-    va_end(args); 
-    return printed_chars;
+va_end(args);
+return (printed_chars);
 }
