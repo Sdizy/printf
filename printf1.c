73302ee8d874a92e4d341f81b815a4636e88eedd
Sdizy <saohatsesamkelo@gmail.com>
2023-09-17 05:36:55 -0700

    now

---
+++ printf1.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 20;
+	int num2 = 14;
+
+	printf("First number: %d\n", num1);
+	printf("Second number: %i\n", num2);
+
+	return (0);
+}
