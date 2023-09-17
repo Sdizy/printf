eb445b4ca200736d17fda17e8fb17a167e0bbd9c
Kisakye19 <kisakyepesther@gmail.com>
2023-09-17 14:13:44 -0700

    question 1

--- format_handlers.c
+++ format_handlers.c
@@ -5,44 +5,44 @@
 /**handle characters*/
 int handle_char(va_list args)
 {
-	char c = va_arg(args, int);
-	putchar(c);
-	return 1;
+char c = va_arg(args, int);
+putchar(c);
+return 1;
 }
 
 /**handle string*/
 int handle_string(va_list args)
 {
-	const char *str = va_arg(args, const char *);
-	if (str != NULL)
-	{
-		fputs(str, stdout);
-		return strlen(str);
-	}
-	return 6; 
+const char *str = va_arg(args, const char *);
+if (str != NULL)
+{
+fputs(str, stdout);
+return strlen(str);
+}
+return 6;
 }
 
 /*handle percent*/
 int handle_percent(va_list args)
 {
-	(void)args;
-	putchar('%');
-	return 1;
+(void)args;
+putchar('%');
+return 1;
 }
 
 /** Define a new function to handle unsupported format specifiers*/
-int handle_unsupported_specifier(const char *ptr) 
+int handle_unsupported_specifier(const char *ptr)
 {
-    putchar('%');
-    putchar(*ptr);
-    return 2; 
+putchar('%');
+putchar(*ptr);
+return 2;
 }
 
 /*for d and i specifiers*/
 int handle_int_specifier(va_list args)
 {
-    int num = va_arg(args, int);
+int num = va_arg(args, int);
 
-    printf("%d", num);
-    return 1;
+printf("%d", num);
+return 1;
 }
--- main.c
+++ main.c
@@ -1,26 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 int main() 
 {
     const char *message = "Hello, world!";
     char character = 'A';
-
+    int x = 42;
+    int y = -123;
+    
     int printed_chars = _printf("Message: %s, Character: %c, Percent: %%\n", message, character);
     printf("Message: %s, Character: %c, Percent: %%\n", message, character);
-
-    _printf("Total characters printed: %d\n", printed_chars);
+    _printf("Testing integer specifiers: %d, %i\n", x, y);
     printf("Total characters printed: %d\n", printed_chars);
 
     return 0;
 }
 
-/*To handle d and i specifiers*/
-int handle()
-{
-    int x = 42;
-    int y = -123;
-
-    _printf("Testing integer specifiers: %d, %i\n", x, y);
-
-    return 0;
-}
