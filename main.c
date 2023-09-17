d25b79053fc56e790f94a99644e1eb3b305cd2cf
Kisakye19 <kisakyepesther@gmail.com>
2023-09-17 04:49:53 -0700

    question 0

--- main.c
+++ main.c
@@ -6,7 +6,9 @@ int main()
     char character = 'A';
 
     int printed_chars = _printf("Message: %s, Character: %c, Percent: %%\n", message, character);
+    printf("Message: %s, Character: %c, Percent: %%\n", message, character);
 
+    _printf("Total characters printed: %d\n", printed_chars);
     printf("Total characters printed: %d\n", printed_chars);
 
     return 0;
