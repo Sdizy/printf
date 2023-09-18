a50dff3cdf0551b6b76a5890af0e6e38f17ed032
Kisakye19 <kisakyepesther@gmail.com>
2023-09-18 07:36:30 -0700

    corrections

--- _printf.c
+++ _printf.c
@@ -1,21 +1,12 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * _printf - Printf function
  * @format: format
- * @fmt: format
  * @args: argument
  * Return: Printed chars.
  */
-int process_format(const char *fmt, va_list args)
-=======
- * _printf - printf 
- * @format: format string printed
- * Return: number of characters printed
- */
 
->>>>>>> refs/remotes/origin/master
 int _printf(const char *format, ...)
 {
 	int chars_printed;
@@ -41,36 +32,9 @@ int _printf(const char *format, ...)
 	if (format == NULL)
 		return (-1);
 
-<<<<<<< HEAD
-	printed_chars += process_format(format, args);
-
-=======
 	va_start(args, format);
 	chars_printed = process_format(format, format_list, args);
->>>>>>> refs/remotes/origin/master
+
 	va_end(args);
 	return (chars_printed);
-}
-
-/**
- * _printf - printf function
- * @format: format
- * Return: number of chars
- */
-
-int _printf(const char *format, ...)
-{
-	va_list args;
-	va_start(args, format);
-	
-	int printed_chars = process_format(format, args);
-
-	va_end(args);
-	return printed_chars;
-}
-
-int main(void)
-{
-	_printf("%d\n", 98);
-	return (0);
 }
